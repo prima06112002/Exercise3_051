f48ad77ed2895d98b74669b77c0392383fd21924
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-25 16:46:23 +0700

    add list menu

--- Exercise3_051/Program.cs
+++ Exercise3_051/Program.cs
@@ -74,7 +74,20 @@ namespace Exercise3_051
         {
             CircularList obj = new CircularList();
             while (true)
-            { }
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. View all the records in the list");
+                    Console.WriteLine("2. Search for a record in the list");
+                    Console.WriteLine("3. Display the first record in the list");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    { }
+                }
+            }
         }
     }
 }
