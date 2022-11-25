443c55ab771f041b6595c4607daea29968a99687
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-25 16:33:04 +0700

    add public void travesrse

--- Exercise3_051/Program.cs
+++ Exercise3_051/Program.cs
@@ -43,5 +43,24 @@ namespace Exercise3_051
             else
                 return false;
         }
+
+        public void traverse()/*Traverses all the nodes of the list*/
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode !=  LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "   " +
+                        currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "   " + LAST.name + "\n");
+            }
+        }
     }
 }
