f63adf20c960d65fbf538f7d49063bc3d3c49785
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-25 16:58:48 +0700

    add case 1 and 2

--- Exercise3_051/Program.cs
+++ Exercise3_051/Program.cs
@@ -85,7 +85,35 @@ namespace Exercise3_051
                     Console.Write("\nEnter your choice (1-4): ");
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
-                    { }
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nlist is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: "  + 
+                                        curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break ;
+                    }
                 }
             }
         }
