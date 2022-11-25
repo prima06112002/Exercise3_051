da334c8004c0da8102560836c179c4b156082044
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-25 17:25:44 +0700

    add public void addNote and delnode

--- Exercise3_051/Program.cs
+++ Exercise3_051/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exercise3_051
 {
@@ -21,8 +22,74 @@ namespace Exercise3_051
         {
             LAST = null;
         }
+        public void addNote()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student :");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll number of the student :");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            if ((LAST == null) || rollNo <= LAST.rollNumber)
+            {
+                if ((LAST != null) && (rollNo == LAST.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = LAST;
+                LAST = newnode;
+                return;
+            }
 
-        public bool Searches(int rollNo, ref Node previous, ref Node current)//
+            Node previous, current;
+            previous = LAST;
+            current = LAST;
+
+            while ((current != null) && (rollNo == current.rollNumber))
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            newnode.next = current;
+            previous.next = newnode;
+        }
+
+        public bool delnode(int rollNO)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNO, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == LAST)
+                LAST = LAST.next;
+            return true;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            previous = LAST;
+            current = LAST;
+            while ((current != null) && (rollNo != current.rollNumber))
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (current == null)
+                return false;
+            else
+                return true;
+        }
+
+            public bool Searches(int rollNo, ref Node previous, ref Node current)//
                                                                              //*Searches for the specified node*/
         {
             for (previous = current = current = LAST.next; current != LAST; previous =
@@ -102,7 +169,7 @@ namespace Exercise3_051
                                 prev = curr = null;
                                 Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
                                 int num = Convert.ToInt32(Console.ReadLine());
-                                if (obj.Search(num, ref prev, ref curr) == false)
+                                if (obj.Searches(num, ref prev, ref curr) == false)
                                     Console.WriteLine("\nRecord not found");
                                 else
                                 {
